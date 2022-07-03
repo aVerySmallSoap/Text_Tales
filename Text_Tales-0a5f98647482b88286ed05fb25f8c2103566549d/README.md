8666ea3fd1fa45a6f061f506c191e69139ec360a
Lirys <iamyourmarkieboo@gmail.com>
2022-07-03 18:39:39 +0800

    Ash

--- Text_Tales-0a5f98647482b88286ed05fb25f8c2103566549d/README.md
+++
@@ -1,2 +0,0 @@
-# Text_Tales
-This ReadMe files is not yet finished.
--- Text_Tales-0a5f98647482b88286ed05fb25f8c2103566549d/src/Main.java
+++
@@ -1,9 +0,0 @@
-import com.LirysBoo.Logic.GameLogic;
-
-public class Main {
-    protected static GameLogic logic = new GameLogic();
-
-    public static void main(String[] args) {
-        logic.gameRunning();
-    }
-}
